9f1e8525a71f99887837f95c7fbd1bea515e1b96
Sakhile Simelane <51701132+sakhike1@users.noreply.github.com>
2023-01-23 15:03:30 +0300

    update

--- components/ScrollUp.vue
+++ components/ScrollUp.vue
@@ -3,7 +3,7 @@
     class="invisible sticky w-full flex pt-5 justify-end bottom-0 pb-3 pr-4 lg:pr-16 transition">
     <div class="text-gray-400 hover:text-blue-400 transition">
       <button @click="scrollToTop">
-        <img class="hover:-translate-y-1 hover:scale-110" src="~/assets/arrow-up (4) 355.png" alt="" />
+        <img class="hover:-translate-y-1 hover:scale-110" src="~/assets/arrow.png" alt="" />
       </button>
     </div>
   </div>
