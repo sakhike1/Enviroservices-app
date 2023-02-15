20649180f2be399a92c44642a60338dddc1cf8aa
Sakhile Simelane <51701132+sakhike1@users.noreply.github.com>
2023-02-16 00:01:05 +0300

    update

--- components/ContactPage.vue
+++ components/ContactPage.vue
@@ -1,10 +1,18 @@
 <template>
 
-    <section class="min-h-screen bg-cover" style="
-      background-image: url('https://images.unsplash.com/photo-1563986768609-322da13575f3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80');
-    ">
-        <div class="flex flex-col min-h-screen bg-black/60">
-            <div class="container flex flex-col flex-1 px-6 py-12 mx-auto">
+    <section class="hover:bg-gray-900">
+        <div class="relative overflow-hidden hover:bg-gray-900 bg-no-repeat bg-cover" style="
+            ">
+            <img src="~/assets/bg.png" alt="" style=" height: 900px; background-position: 100%; width: 100%;">
+
+
+
+
+
+
+            <div class="absolute pt-20 top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed"
+                style="background-color: rgba(0, 0, 0, 0.55)">
+
                 <div class="flex-1 lg:flex lg:items-center lg:-mx-6">
                     <div data-aos="fade-up" data-aos-duration="3000" class="text-white lg:w-1/2 lg:mx-6">
                         <h1 class="text-3xl font-semibold capitalize lg:text-4xl">
@@ -16,18 +24,19 @@
                             keep a watch on the toxic compounds we ingest that pollute the
                             atmosphere. We need to adopt environmentally sustainable methods
                             to minimise
+                            <Nuxt-link to="Sales">
+                                <button
+                                    class="max-w-xl text-xs text-gray-200 mt-6 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-base px-6 py-3.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                    Give us a call
+                                </button>
+                            </Nuxt-link>
                         </p>
-                        <Nuxt-link to="Sales">
-                            <button
-                                class="px-8 py-3 mt-6 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-50">
-                                Give us a call
-                            </button>
-                        </Nuxt-link>
+
                     </div>
 
                     <div class="mt-8 lg:w-1/2 lg:mx-6">
                         <div data-aos="fade-up" data-aos-duration="3000"
-                            class="w-full px-8 py-10 mx-auto overflow-hidden bg-white shadow-2xl rounded-xl dark:bg-gray-900 lg:max-w-xl">
+                            class="w-full px-10 py-10 mx-auto overflow-hidden bg-white shadow-2xl rounded-xl dark:bg-gray-900 lg:max-w-xl">
                             <h1 class="text-2xl font-medium text-gray-700 dark:text-gray-200">
                                 Contact Us
                             </h1>
@@ -38,7 +47,8 @@
 
                             <form @submit.prevent="submitForm" class="mt-6">
                                 <div class="flex-1">
-                                    <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Full Name</label>
+                                    <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Full
+                                        Name</label>
                                     <input type="text" v-model="name" required placeholder="Name"
                                         class="block w-full px-5 py-3 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
                                 </div>
@@ -65,8 +75,10 @@
                         </div>
                     </div>
                 </div>
+
             </div>
         </div>
+        <div />
     </section>
     <!-- Container for demo purpose -->
 </template>
@@ -83,7 +95,7 @@ export default {
     },
     data() {
         return {
-            formdata: {
+            FormData: {
                 name: '',
                 email: '',
                 message: ''
@@ -96,7 +108,7 @@ export default {
             const supabase = createClient('https://fkwcupzorassszwjghru.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZrd2N1cHpvcmFzc3N6d2pnaHJ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzUxMTI5OTgsImV4cCI6MTk5MDY4ODk5OH0.9q6voKu1B0YCBeHQ6rPt6peGSxSxZTeGtq38x3X_gdw')
             await supabase.from('contacts').insert({ name: this.name, email: this.email, message: this.message })
             alert('Message sent thank you !')
-            this.$router.push('/')
+            this.$router.push('home')
         }
     }
 }
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/supabase"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/supabase", "@nuxtjs/axios"],
 
   build: {
     transpile: ["gsap"],
